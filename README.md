59dfdc344d09e3804bf14f5ddbb4b538f93af37f
IvanaVujovi <113903268+IvanaVujovi@users.noreply.github.com>
2022-09-23 10:44:16 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-ifrxfi
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-ifrxfi)
---
+++ index.js
@@ -0,0 +1,46 @@
+//let a=prompt("unesi neki broj");
+//b=a%2;
+//if(b==0){
+//console.log(a+" je paran broj.")
+//}else{
+ // console.log(a+" je neparan broj.")
+//};
+
+//let a=prompt("unesi prvi broj");
+//let b=prompt("unesi drugi broj");
+//if(a>b){
+  //console.log(a+" je veci.")
+//}else if(a==b){
+ // console.log("Jednaki su.")
+//}else{
+ // console.log(b+" je veci.")
+//};
+//let a=prompt("unesi prvi broj");
+//let b=prompt("unesi drugi broj");
+//let c=prompt("unesi treci broj");
+//if(a>b && a>c) {
+  //console.log(a+" je najveci.")
+//}else if(b>a && b>c) {
+ // console.log(b+" je najveci.")
+//}else if(c>a && c>b) {
+ // console.log(c+" je najveci.")
+//}else if(b>a && b==c) {
+ // console.log(b+" i "+c+" su najveci.")
+//}else if(b>c && b==a) {
+ // console.log(b+" i "+a+" su najveci.")
+//}else if(a>b && c==a) {
+ // console.log(c+" i "+a+ " su najveci.")
+//}else if(c==b && a==c) {
+  //console.log("Jednaki su.")
+//};
+
+let a=prompt("unesi godinu");
+if(a%4!=0){
+  console.log(a+" nije prestupna.")
+}else if(a%100!=0){
+  console.log(a+" je prestupna.")
+}else if(a%400!=0){
+  console.log(a+" nije prestupna.")
+}else{
+console.log(a+" je prestupna.")
+}
